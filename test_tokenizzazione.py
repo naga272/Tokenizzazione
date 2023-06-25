b00b1f0ef1b3b037985c6b79b286f55407eb11f1
naga272 <114398392+naga272@users.noreply.github.com>
2023-06-25 14:29:48 +0200

    Add files via upload

--- test_tokenizzazione.py
+++ test_tokenizzazione.py
@@ -1,6 +1,11 @@
 import re
 
 
+#COSTANTI
+EXIT_SUCCESS = 0
+EXIT_FAILED = 1
+
+
 def rimuovi_elementi(x):
     try:
         for i in range(0, len(x), 1):
@@ -10,7 +15,6 @@ def rimuovi_elementi(x):
 
 
 def main(stri): #compile e match
-    
     #pattern per requisiti stringa
     reg1 = re.compile(r"\s*\d+\s*[/*+%\-\(\)]*\s*\d+\s*")
     
@@ -18,12 +22,12 @@ def main(stri): #compile e match
         print("syntax error!")
         return False
     
-    a = re.split(r"[/\*\-\+\s()]\s*", stri) # contenitore numeri
+    a = re.split(r"[/\*\-\+\s()%]\s*", stri) # contenitore numeri
     b = re.split(r"[\d\s]\s*", stri) # contenitore operandi / parentesi
     '''
         *
         *  Uso la funzione rimuovi_elementi(x) per togliere dalle due liste gli elementi vuoti
-        *  che si vanno a generano dal re.split()
+        *  che si vanno a generare dal re.split()
         *
     '''
     a = rimuovi_elementi(a) 
@@ -47,59 +51,64 @@ def main(stri): #compile e match
         d.append(a[i])
         a.remove(a[i])
         b.remove(b[i])
-        a.insert(z - 1, risolvi(d, c))
+
+        # print(c) # c gli operatori dentro alle parentesi
+        a.insert(z, risolvi(d, c)) # d contine numeri dentro alla parentesi
+
+        print(stri) # input 
+        print(b)    # contiene gli operatori fuori dalle parentesi tonde
+        print(a)    # contiene gli operandi
+        print(c)    # contiene gli operatori all'interno delle parentesi tonde
+        print(d)    # contiene gli operandi all'interno delle parentesi tonde 
     return risolvi(a, b)
 
 
 def risolvi(a, d):
-    i = 0
-    while i < len(d):
-        if '*' in d:
-            z = 0
-            while d[z] != '*':
-                z += 1
-            if d[z] == '*':
-                d.remove(d[z])
-                a[z] = str(int(a[z]) * int(a[z + 1]))
-                a.remove(a[z + 1])
-                
-        if '%' in d:
-            z = 0
-            while d[z] != '%':
-                z += 1
-            if d[z] == '%':
-                a.remove(a[z + 1])
-                d.remove(d[z])
-                a[z] = str(int(a[z]) % int(a[z + 1]))
-                a.remove(a[z + 1])
+    while len(d) > 0:
+        while '*' in d or '/' in d or '%' in d:
+            c = 0
+            while d[c] != '*' and d[c] != '/' and d[c] != '%':
+                c += 1
 
-        if '/' in d:
-            z = 0
-            while d[z] != '/':
-                z += 1
+            if len(d) > 0:
+                if d[c] == '*':
+                    a[c] = str(int(a[c]) * int(a[c + 1]))
+                    a.remove(a[c + 1])
+                    d.remove(d[c])
 
-            if d[z] == '/':
-                d.remove(d[z])
-                a[z] = str(int(a[z]) // int(a[z + 1]))
-                a.remove(a[z + 1])
-
-        if '*' not in d and '/' not in d and '%' not in d:
             try:
-                if len(d) >= 1:
-                    if d[0] == '+':
-                        d.remove(d[0])
-                        a[0] = str(int(a[0]) + int(a[1]))
-                        a.remove(a[1])
-                    if d[0] == '-':
-                        d.remove(d[0])
-                        a[0] = str(int(a[0]) - int(a[1]))
-                        a.remove(a[1])
+                if len(d) > 0:
+                    if d[c] == '/':
+                        a[c] = str(int(a[c]) // int(a[c + 1]))
+                        a.remove(a[c + 1])
+                        d.remove(d[c])
+
+                    if d[c] == '%':
+                        a[c] = str(int(a[c]) % int(a[c + 1]))
+                        a.remove(a[c + 1])
+                        d.remove(d[c])
+                else:
+                    return int(a[0])
             except Exception:
-                return int(a[0])
-    return int(a[0])
+                return a[0]
+        
+        try:
+            while len(d) > 0:
+                if d[c] == '+':
+                    a[c] = str(int(a[c]) + int(a[c + 1]))
+                    a.remove(a[c + 1])
+                    d.remove(d[c])
+
+                if d[c] == '-':
+                    a[c] = str(int(a[c]) - int(a[c + 1]))
+                    a.remove(a[c + 1])
+                    d.remove(d[c])
+        except Exception:
+            return a[0]
+    return a[0]
 
 
 if __name__ == "__main__":
-    a = ['22', '33', '5']
-    d = ['*', '-']
-    print(main("22 * 3 / 12 % 5"))
+    a = ['22', '33', '5', '6']
+    d = ['*', '-', '/']
+    print(main("22 * (3 + 12) % 5")) #main("22 * 3 / 12 % 5")
